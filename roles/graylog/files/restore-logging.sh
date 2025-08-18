9ac8f7c00319df8247e9e0d6fae93571f8e7ed3f
IldarMinaev <ildar.minaev.qubership@gmail.com>
2025-08-18 14:49:58 +0400

    chore: fix linters foundings

--- roles/graylog/files/restore-logging.sh
+++ roles/graylog/files/restore-logging.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 restore_vault=$1
 status_file=/srv/docker/graylog/restore.successful
-echo 'restore from' $restore_vault
+echo 'restore from' "$restore_vault"
 sudo docker stop graylog_web_1 graylog_graylog_1 graylog_elasticsearch_1 graylog_storage_1 graylog_mongo_1
 sudo rm -rf /srv/docker/graylog/*
-curl -X POST -v -H "Content-Type: application/json" -d '{"vault":"'$restore_vault'", "dbs":["db1"]}' localhost:8080/restore
+curl -X POST -v -H "Content-Type: application/json" -d '{"vault":"'"$restore_vault"'", "dbs":["db1"]}' localhost:8080/restore
 
 until [ -f $status_file ]; do
     echo 'wait for restore command ending'
